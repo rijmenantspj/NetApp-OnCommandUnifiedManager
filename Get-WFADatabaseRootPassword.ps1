2d611c563320e0a461e7e0b24e7f8b075ceac3fe
Pieter-Jan Rijmenants <rijmenants.pj@gmail.com>
2018-01-18 16:05:07 +0100

    Created PowerShell Script "Get-WFADatabaseRootPassword.ps1"

---
+++ Get-WFADatabaseRootPassword.ps1
@@ -0,0 +1,10 @@
+﻿$REGISTERY_KEY = "HKLM:\SOFTWARE\Wow6432Node\Apache Software Foundation\Procrun 2.0\NA_WFA_SRV\Parameters\Java"
+
+try{
+    $Registery = Get-ItemProperty $REGISTERY_KEY -ErrorAction Stop
+    $WFADatabaseRootPassword = ($Registery | Select -ExpandProperty Options | Where {$_ -match "-Dmysql.password"}).split("=")[1]
+}catch [Exception]{
+    Write-Error $_.Exception.Message
+}
+
+Write-Host "Password: $WFADatabaseRootPassword"
